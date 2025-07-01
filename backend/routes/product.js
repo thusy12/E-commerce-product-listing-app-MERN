3c99c29a2d8c355921d74d8ab0aca6f59c7378b6
thusy <12thusy1999@gmail.com>
2025-07-02 00:11:32 +0530

    Updates to delete product and formatting routes

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { route } = require('../app');
-const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controllers/productController');
+const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController');
 const router = express.Router();
 
 router.route('/products').get(getProducts);
 router.route('/product/new').post(newProduct);
-router.route('/product/:id').get(getSingleProduct);
-router.route('/product/:id').put(updateProduct);
+router.route('/product/:id')
+                            .get(getSingleProduct)
+                            .put(updateProduct)
+                            .delete(deleteProduct);
 
 module.exports = router;
