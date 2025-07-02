6b7e4014aab6afb952b3005aafebd73db903832e
thusy <12thusy1999@gmail.com>
2025-07-02 21:46:26 +0530

    Updates to search product name regex

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,13 @@
 const Product = require('../models/productModel');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncError = require('../middlewares/catchAsyncError');
+const ApiFeatures = require('../utils/apiFeatures');
 
 //Get products = /api/v1/products
 exports.getProducts = async (req,res,next)=>{
-    const products = await Product.find();
+    const apiFeatures = new ApiFeatures(Product.find(), req.query).search();
+    const products = await apiFeatures.query;
+    
     res.status(200).json({
         success:true,
         count:products.length,
