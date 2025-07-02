6b8d40c39cbb5382488bb0a71dee4e60a3999724
thusy <12thusy1999@gmail.com>
2025-07-02 21:08:52 +0530

    Handle validation errors

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,20 +1,30 @@
 const { stack } = require("../app");
+const ErrorHandler = require('../utils/errorHandler');
 
 module.exports = (err, req, res, next)=>{
     err.statusCode = err.statusCode || 500;
 
     if(process.env.NODE_ENV == 'development'){
-        return res.status(err.statusCode).json({
+        res.status(err.statusCode).json({
             success: false,
             message: err.message,
-            stack: err.stack
+            stack: err.stack,
+            error: err
         })
     }
     
     if(process.env.NODE_ENV == 'production'){
-        return res.status(err.statusCode).json({
+        let message = err.message;
+        let error = {...err};
+
+        if(err.name == "ValidationError"){
+            message = Object.values(err.errors).map(value => value.message);
+            error = new ErrorHandler(message, 400);
+        }
+
+        res.status(err.statusCode).json({
             success: false,
-            message: err.message,
+            message: error.message || "Internal Server Error",
         })
     }
 }
