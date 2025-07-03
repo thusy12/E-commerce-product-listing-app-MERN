52178f1e29f8167517dda3659d0ded6a9cb3d09b
thusy <12thusy1999@gmail.com>
2025-07-03 23:54:21 +0530

    Updates to protect routes from unauthorized users

---
+++ backend/middlewares/authenticate.js
@@ -0,0 +1,17 @@
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncError = require("./catchAsyncError");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
+    const {token} = req.cookies;
+
+    // Check if token exists in cookies
+    if (!token) {
+        return next(new ErrorHandler("Please login to access this resource", 401));
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id)
+    next();
+});
