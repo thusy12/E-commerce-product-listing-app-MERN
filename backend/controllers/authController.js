688af8f4e13a05bb1eb5312586e1f0277cc26665
thusy <12thusy1999@gmail.com>
2025-07-04 21:17:04 +0530

    Updates to user logout

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -39,3 +39,15 @@ exports.loginUser = catchAsyncError(async (req, res, next) => {
 
     sendToken(user, 201, res, "User logged in successfully");
 })
+
+exports.logoutUser = catchAsyncError(async (req, res, next) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "User logged out successfully"
+    });
+})
