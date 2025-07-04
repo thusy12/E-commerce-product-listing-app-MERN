66b19eec237eaff263355b60ad6a37eb6800beb7
thusy <12thusy1999@gmail.com>
2025-07-04 22:26:17 +0530

    Updates to generate reset password token and send it in email

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const catchAsyncError = require("../middlewares/catchAsyncError");
 const User = require("../models/userModel");
+const sendEmail = require("../utils/email");
 const ErrorHandler = require("../utils/errorHandler");
 const sendToken = require("../utils/jwt");
 
@@ -50,4 +51,39 @@ exports.logoutUser = catchAsyncError(async (req, res, next) => {
         success: true,
         message: "User logged out successfully"
     });
+})
+
+exports.forgotPassword = catchAsyncError(async (req, res, next) => {
+    const user = await User.findOne({email:req.body.email});
+
+    if(!user){
+        return next(new ErrorHandler("User not found with this email", 404));
+    }
+
+    const resetToken = user.getResetToken();
+    await user.save({validateBeforeSave: false});
+
+    // Create reset password URL
+    const resetUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`;
+    const message = `Your password reset token is as follows:\n\n${resetUrl}\n\nIf you have not requested this, please ignore this email.`;
+
+    try {
+        // Send email with reset token
+        await sendEmail({
+            email: user.email,
+            subject: "Listing cart password Recovery",
+            message: message
+        })
+
+        res.status(200).json({
+            success: true,
+            message: `Email sent to ${user.email} successfully`
+        });
+    } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordTokenExpire = undefined;
+        await user.save({ validateBeforeSave: false });
+
+        return next(new ErrorHandler(error.message, 500));
+    }
 })
