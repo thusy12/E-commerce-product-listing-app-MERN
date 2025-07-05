4703bfe5ae8a46fe0852195df3557b715a57f698
thusy <12thusy1999@gmail.com>
2025-07-05 19:49:06 +0530

    Updates to admin get all order datas

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { isAuthenticatedUser } = require('../middlewares/authenticate');
-const { newOrder, getSingleOrder, myOrders } = require('../controllers/orderController');
+const { isAuthenticatedUser, authorizedRoles } = require('../middlewares/authenticate');
+const { newOrder, getSingleOrder, myOrders, orders } = require('../controllers/orderController');
 const router = express.Router();
 
 router.route('/order/new').post(isAuthenticatedUser, newOrder);
 router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
 router.route('/myorders').get(isAuthenticatedUser, myOrders);
+
+// Admin routes
+router.route('/admin/orders').get(isAuthenticatedUser, authorizedRoles('admin','super admin'), orders);
 
 module.exports = router;
