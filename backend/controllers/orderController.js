f7de911e283ef00bb34be25addf5a79d047b4571
thusy <12thusy1999@gmail.com>
2025-07-05 19:40:31 +0530

    Updates to get all orders of logged user

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -44,3 +44,13 @@ exports.getSingleOrder = catchAsyncError(async(req, res, next)=>{
         order
     });
 })
+
+//Get logged in user orders = /api/v1/myorders
+exports.myOrders = catchAsyncError(async(req, res, next) => {
+    const orders = await Order.find({ user: req.user.id });
+
+    res.status(200).json({
+        success: true,
+        orders
+    });
+})
