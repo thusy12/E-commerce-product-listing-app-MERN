2703fbaacdaeaced2440f140d3da74c9b9ef640c
thusy12 <12thusy1999@gmail.com>
2025-07-06 00:43:27 +0530

    Add header and footer parts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Footer from './components/layouts/Footer';
+import Header from './components/layouts/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Testing...</h1>
+      <Header/>
+      <Footer/>
     </div>
   );
 }
