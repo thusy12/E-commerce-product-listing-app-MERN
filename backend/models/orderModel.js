b31b2ac86bb1424d2c904457ef39ad11d18fd0e7
thusy <12thusy1999@gmail.com>
2025-07-05 18:54:44 +0530

    Create orders model

---
+++ backend/models/orderModel.js
@@ -0,0 +1,92 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    shippingInfo: {
+        address: {
+            type: String,
+            required: true
+        },
+        country: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        postalCode: {
+            type: Number,
+            required: true
+        },
+        phoneNo: {
+            type: Number,
+            required: true
+        }
+    },
+    user: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    orderItems:[{
+        name: {
+            type: String,
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        product: {
+            type: mongoose.SchemaTypes.ObjectId,
+            ref: 'Product',
+            required: true
+        }
+    }],
+    itemsPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    taxPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    shippingPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    paidAt: {
+        type: Date
+    },
+    deliveredAt: {
+        type: Date
+    },
+    orderStatus: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+let orderModel = mongoose.model('Order', orderSchema);
+
+module.exports = orderModel;
