e32fb8ee6b8f6a1a68e0ea64ed6a52010793eda5
thusy12 <12thusy1999@gmail.com>
2025-07-07 20:54:19 +0530

    Updates to create products reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,9 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
+import productsReducer from './slices/productsSlice';
 
 const reducer = combineReducers({
-
+    productsState: productsReducer,
 })
 
 const store = configureStore({
