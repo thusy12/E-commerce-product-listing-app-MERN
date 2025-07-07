65b805988457d6fc561f2c988a90205cf59a6522
thusy12 <12thusy1999@gmail.com>
2025-07-08 00:21:17 +0530

    Updates to show product details with redux state

--- frontend/src/components/product/Product.js
+++ frontend/src/components/product/Product.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 export default function Product({product}){
     return (
       <div className="col-sm-12 col-md-6 col-lg-3 my-3">
@@ -5,7 +6,7 @@ export default function Product({product}){
           <img className="card-img-top mx-auto" src={product.images[0].image} />
           <div className="card-body d-flex flex-column">
             <h5 className="card-title">
-              <a href="">{product.name}</a>
+              <Link to={`/product/${product._id}`}>{product.name}</Link>
             </h5>
             <div className="ratings mt-auto">
               <div className="rating-outer">
@@ -19,9 +20,7 @@ export default function Product({product}){
               <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
             </div>
             <p className="card-text">{product.price}</p>
-            <a href="#" id="view_btn" className="btn btn-block">
-              View Details
-            </a>
+            <Link to={`/product/${product._id}`} id="view_btn" className="btn btn-block">View Details</Link>
           </div>
         </div>
       </div>
