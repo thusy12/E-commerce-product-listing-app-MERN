05bb61fe39edc1b0999a1d7a870c12a1098d5540
thusy12 <12thusy1999@gmail.com>
2025-07-07 22:30:09 +0530

    Updates to showing error alert

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import Footer from './components/layouts/Footer';
 import Header from './components/layouts/Header';
 import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 import {HelmetProvider} from 'react-helmet-async';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <div className="App">
         <HelmetProvider>
           <Header />
+          <ToastContainer theme='dark'/>
           <Routes>
             <Route path="/" element={<Home />}></Route>
           </Routes>
