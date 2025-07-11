18db599d7bc9a5633c710b5bb176a2a8c59b9227
thusy12 <12thusy1999@gmail.com>
2025-07-11 21:02:05 +0530

    Updates to login reducer, action

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+import { loginFail, loginRequest, loginSuccess } from "../slices/authSlice"
+
+export const login = (email, password) => async(dispatch) => {
+    try {
+        dispatch(loginRequest())
+        const {data} = await axios.post('/api/v1/login', {email, password});
+        dispatch(loginSuccess(data))
+    } catch (error) {
+        //handle error
+        dispatch(loginFail(error.response.data.message))
+    }
+}
---
+++ frontend/src/slices/authSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+    name:'auth',
+    initialState:{
+        loading:false,
+        isAuthenticated:false
+    },
+    reducers:{
+        loginRequest(state, action){
+            return{
+                ...state,//copy old state values
+                loading:true
+            }
+        },
+        loginSuccess(state, action){
+            return{
+                loading:false,
+                isAuthenticated:true,
+                user:action.payload.user
+            }
+        },
+
+        loginFail(state,action){
+            return{
+                ...state,
+                loading:false,
+                error:action.payload
+            }
+        }
+    }
+});
+
+const {actions, reducer} = authSlice;
+
+export const {loginRequest, loginSuccess, loginFail} = actions;
+
+export default reducer;
