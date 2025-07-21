1ea20fa85e7499c5831a6565812e6b2eb97383fd
thusy12 <12thusy1999@gmail.com>
2025-07-21 23:24:33 +0530

    Updates to review reducer, actions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { productsFail, productsRequest, productsSuccess } from '../slices/productsSlice';
-import { productFail, productRequest, productSuccess } from '../slices/productSlice';
+import { createReviewFail, createReviewRequest, createReviewSuccess, productFail, productRequest, productSuccess } from '../slices/productSlice';
 
 export const getProducts = (keyword, price, category, rating, currentPage) => async (dispatch)=>{
     try {
@@ -39,5 +39,21 @@ export const getProduct = id => async (dispatch)=>{
     } catch (error) {
         //handle error
         dispatch(productFail(error.response.data.message))
+    }
+}
+
+export const createReview = reviewData => async (dispatch)=>{
+    try {
+        dispatch(createReviewRequest())
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        const {data} = await axios.put(`/api/v1/review`,reviewData, config);
+        dispatch(createReviewSuccess(data))
+    } catch (error) {
+        //handle error
+        dispatch(createReviewFail(error.response.data.message))
     }
 }
--- frontend/src/slices/productSlice.js
+++ frontend/src/slices/productSlice.js
@@ -4,24 +4,60 @@ const productSlice = createSlice({
     name:'product',
     initialState: {
         loading:false,
-        product:{}
+        product:{},
+        isReviewSubmitted:false,
     },
     reducers:{
         productRequest(state,action){
             return{
+                ...state,
                 loading:true
             }
         },
         productSuccess(state,action){
             return{
+                ...state,
                 loading:false,
                 product:action.payload.product
             }
         },
         productFail(state,action){
             return{
+                ...state,
                 loading:false,
                 error:action.payload
+            }
+        },
+        createReviewRequest(state,action){
+            return{
+                ...state,
+                loading:true
+            }
+        },
+        createReviewSuccess(state,action){
+            return{
+                ...state,
+                loading:false,
+                isReviewSubmitted:true,
+            }
+        },
+        createReviewFail(state,action){
+            return{
+                ...state,
+                loading:false,
+                error:action.payload
+            }
+        },
+        clearReviewSubmitted(state,action){
+            return{
+                ...state,
+                isReviewSubmitted:false
+            }
+        },
+        clearError(state, action) {
+            return {
+                ...state,
+                error: null
             }
         }
     }
@@ -29,6 +65,15 @@ const productSlice = createSlice({
 
 const {actions, reducer} = productSlice;
 
-export const {productRequest, productSuccess, productFail} = actions;
+export const {
+    productRequest, 
+    productSuccess, 
+    productFail,
+    createReviewRequest,
+    createReviewSuccess,
+    createReviewFail,
+    clearReviewSubmitted,
+    clearError
+} = actions;
 
 export default reducer;
